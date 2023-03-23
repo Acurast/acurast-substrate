967b9efd0102f5cc60e80733862f23ac26632a22
Simon Wehrli <simon@smartnuance.com>
2023-03-23 12:31:55 +0000

    benchmarking with BenchmarkHelper
    instead of relying on
    - From<JobRequirementsFor<...>> trait (not implementable from outside crate)
    - Default trait (not implementable from outside crate)

--- runtime/src/benchmarking.rs
+++ runtime/src/benchmarking.rs
@@ -1,11 +1,31 @@
-use crate::{AcurastAsset, RegistrationExtra};
-use pallet_acurast_marketplace::JobRequirements;
+use crate::{AcurastAsset, AcurastAssetId, Extra, RegistrationExtra};
+use pallet_acurast_marketplace::{JobRequirements, MultiDestination};
 use sp_runtime::AccountId32 as AccountId;
 use xcm::{
 	latest::prelude::{X2, X3},
 	prelude::{Concrete, Fungible, GeneralIndex, PalletInstance, Parachain},
 	v2::{MultiAsset, MultiLocation},
 };
+
+pub struct AcurastBenchmarkHelper;
+
+impl pallet_assets::BenchmarkHelper<codec::Compact<AcurastAssetId>> for AcurastBenchmarkHelper {
+	fn create_asset_id_parameter(id: u32) -> codec::Compact<AcurastAssetId> {
+		let asset_id = AssetId::Concrete(MultiLocation::new(
+			1,
+			X3(Parachain(1000), PalletInstance(50), GeneralIndex(id as u128)),
+		));
+		codec::Compact(asset_id)
+	}
+}
+
+impl pallet_acurast_assets_manager::benchmarking::BenchmarkHelper<codec::Compact<AcurastAssetId>>
+	for AcurastBenchmarkHelper
+{
+	fn manager_account() -> frame_system::pallet::AccountId {
+		[0; 32].into()
+	}
+}
 
 impl From<pallet_acurast_marketplace::benchmarking::MockAsset> for AcurastAsset {
 	fn from(asset: pallet_acurast_marketplace::benchmarking::MockAsset) -> Self {
@@ -19,27 +39,13 @@ impl From<pallet_acurast_marketplace::benchmarking::MockAsset> for AcurastAsset 
 	}
 }
 
-impl From<JobRequirements<AcurastAsset, AccountId>> for RegistrationExtra {
-	fn from(requirements: JobRequirements<AcurastAsset, AccountId>) -> Self {
-		RegistrationExtra {
-			destination: MultiLocation {
+impl pallet_acurast::benchmarking::BenchmarkHelper<crate::Runtime> for AcurastBenchmarkHelper {
+	fn registration_extra() -> Extra {
+		Extra {
+			destination: MultiDestination::Acurast(MultiLocation {
 				parents: 1,
 				interior: X2(Parachain(2001), PalletInstance(40)),
-			},
-			parameters: None,
-			requirements,
-			expected_fulfillment_fee: 200,
-		}
-	}
-}
-
-impl Default for RegistrationExtra {
-	fn default() -> Self {
-		RegistrationExtra {
-			destination: MultiLocation {
-				parents: 1,
-				interior: X2(Parachain(2001), PalletInstance(40)),
-			},
+			}),
 			parameters: None,
 			requirements: JobRequirements {
 				slots: 1,
@@ -50,6 +56,7 @@ impl Default for RegistrationExtra {
 					}),
 					fun: Fungible(20100),
 				}),
+				min_reputation: None,
 				instant_match: None,
 			},
 			expected_fulfillment_fee: 200,

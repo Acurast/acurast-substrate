1143b5a589235ca713be6be2cb29b4fe8e4561c0
Simon Wehrli <simon@smartnuance.com>
2023-05-04 08:39:49 +0200

    integrate hyperdrive outgoing
    * remove local patch
    
    * integrate MMR canonicalization using fork-unique key
    
    * fix offchain-index not activated
    
    * send_test_message extrinsic (root-only)
    
    * extend HyperdriveApi with snapshot_roots
    
    * add HyperdriveApi
    
    * integrate acurast-core #73 and #79
    
    split report extrinsic into two new extrinsics (report and finalize_job)
    
    * integrate hyperdrive outgoing

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -19,10 +19,12 @@
 
 pub mod block_weights;
 pub mod extrinsic_weights;
+pub mod pallet_acurast_hyperdrive_outgoing_weights;
 pub mod paritydb_weights;
 pub mod rocksdb_weights;
 
 pub use block_weights::constants::BlockExecutionWeight;
 pub use extrinsic_weights::constants::ExtrinsicBaseWeight;
+pub use pallet_acurast_hyperdrive_outgoing_weights::TezosHyperdriveOutgoingWeight;
 pub use paritydb_weights::constants::ParityDbWeight;
 pub use rocksdb_weights::constants::RocksDbWeight;

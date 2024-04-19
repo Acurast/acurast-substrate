02b265165465234b778a4c65680a80761e3f3041
Mike Godenzi <m.godenzi@papers.ch>
2024-04-19 15:32:44 +0200

    chore: reverted spec version change

--- runtime/acurast-rococo/src/lib.rs
+++ runtime/acurast-rococo/src/lib.rs
@@ -188,7 +188,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("acurast-parachain"),
 	impl_name: create_runtime_str!("acurast-parachain"),
 	authoring_version: 3,
-	spec_version: 39,
+	spec_version: 19,
 	impl_version: 2,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

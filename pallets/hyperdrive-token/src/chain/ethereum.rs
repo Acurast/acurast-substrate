64aec2e5b3ac71a7b4b977a5d1c0f5a4c5096b22
Simon Wehrli <simon@smartnuance.com>
2025-03-14 09:00:50 +0000

    fix: parse ignoring padding

--- pallets/hyperdrive-token/src/chain/ethereum.rs
+++ pallets/hyperdrive-token/src/chain/ethereum.rs
@@ -1,4 +1,4 @@
-use core::marker::PhantomData;
+use core::{marker::PhantomData, ops};
 use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 
@@ -9,6 +9,26 @@ use pallet_acurast::MultiOrigin;
 
 use super::*;
 use crate::{Action, ActionDecoder, ActionEncoder, RawAction};
+
+const fn action_id_range() -> ops::Range<usize> {
+	3 * 8..4 * 8
+}
+
+const fn amount_range() -> ops::Range<usize> {
+	32 + 3 * 8..32 + 4 * 8
+}
+
+const fn asset_id_range() -> ops::Range<usize> {
+	2 * 32 + 3 * 8..2 * 32 + 4 * 8
+}
+
+const fn transfer_nonce_range() -> ops::Range<usize> {
+	3 * 32 + 3 * 8..3 * 32 + 4 * 8
+}
+
+const fn dest_range() -> ops::Range<usize> {
+	4 * 32 + 12..4 * 32 + 32
+}
 
 #[derive(RuntimeDebug, Encode, Decode, TypeInfo, Clone, Eq, PartialEq)]
 #[scale_info(skip_type_params(AccountConverter))]
@@ -30,9 +50,13 @@ where
 		if encoded.len() != 160 {
 			Err(Self::Error::CouldNotDecodeAction)?;
 		}
-		let raw_action: RawAction = u32::from_be_bytes(encoded[0..4].try_into().unwrap())
-			.try_into()
-			.map_err(|_err| Self::Error::CouldNotDecodeAction)?;
+		let raw_action: RawAction = u32::from_be_bytes(
+			encoded[3 * 8..4 * 8]
+				.try_into()
+				.map_err(|_| ActionDecoderError::CouldNotDecodeAction)?,
+		)
+		.try_into()
+		.map_err(|_err| Self::Error::CouldNotDecodeAction)?;
 
 		fn convert_account_id<Account, AccountConverter: TryFrom<Vec<u8>> + Into<Account>>(
 			bytes: &[u8; 32],
@@ -46,17 +70,31 @@ where
 
 		match raw_action {
 			RawAction::TransferToken => {
-				let amount: u32 = u32::from_be_bytes(encoded[4..8].try_into().unwrap());
-				let asset_id: u32 = u32::from_be_bytes(encoded[8..12].try_into().unwrap());
-				let transfer_nonce = u32::from_be_bytes(encoded[12..16].try_into().unwrap());
-				let transfer_recipient = convert_account_id::<AccountId, AccountConverter>(
-					encoded[16..36].try_into().unwrap(),
+				let amount: u32 = u32::from_be_bytes(
+					encoded[amount_range()]
+						.try_into()
+						.map_err(|_| ActionDecoderError::CouldNotDecodeAction)?,
+				);
+				let asset_id: u32 = u32::from_be_bytes(
+					encoded[asset_id_range()]
+						.try_into()
+						.map_err(|_| ActionDecoderError::CouldNotDecodeAction)?,
+				);
+				let transfer_nonce = u32::from_be_bytes(
+					encoded[transfer_nonce_range()]
+						.try_into()
+						.map_err(|_| ActionDecoderError::CouldNotDecodeAction)?,
+				);
+				let dest = convert_account_id::<AccountId, AccountConverter>(
+					encoded[dest_range()]
+						.try_into()
+						.map_err(|_| ActionDecoderError::CouldNotDecodeAction)?,
 				)?;
 				Ok(Action::TransferToken(
 					amount as u128,
 					(asset_id != 0u32).then_some(asset_id as u128),
 					transfer_nonce,
-					MultiOrigin::Acurast(transfer_recipient),
+					MultiOrigin::Acurast(dest),
 				))
 			},
 			RawAction::Noop => Ok(Action::Noop),
@@ -77,11 +115,12 @@ impl<AccountId> ActionEncoder<AccountId> for EthereumActionEncoder {
 
 					let raw_action: RawAction = action.into();
 					let raw_action_encoded: u32 = raw_action.into();
-					buffer[0..4].copy_from_slice(&raw_action_encoded.to_be_bytes());
-					buffer[4..8].copy_from_slice(&(*amount as u32).to_be_bytes());
-					buffer[8..12].copy_from_slice(&(asset_id.unwrap_or_default()).to_be_bytes());
-					buffer[12..16].copy_from_slice(&transfer_nonce.to_be_bytes());
-					buffer[16..36].copy_from_slice(&account_id.0);
+					buffer[action_id_range()].copy_from_slice(&raw_action_encoded.to_be_bytes());
+					buffer[amount_range()].copy_from_slice(&(*amount as u32).to_be_bytes());
+					buffer[asset_id_range()]
+						.copy_from_slice(&(asset_id.unwrap_or_default()).to_be_bytes());
+					buffer[transfer_nonce_range()].copy_from_slice(&transfer_nonce.to_be_bytes());
+					buffer[dest_range()].copy_from_slice(&account_id.0);
 
 					buffer.to_vec()
 				},

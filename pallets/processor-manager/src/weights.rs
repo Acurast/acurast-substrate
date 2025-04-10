6b60592bfc95bfb2b8827d7dcb0abec762b67ada
Mike Godenzi <m.godenzi@papers.ch>
2025-04-10 11:23:43 +0200

    feat/management-endpoint
    * chore: updated benchmarks
    
    * feat: added management endpoint

--- pallets/processor-manager/src/weights.rs
+++ pallets/processor-manager/src/weights.rs
@@ -251,4 +251,11 @@ impl<T: frame_system::Config> crate::WeightInfo for WeightInfo<T> {
 			.saturating_add(Weight::from_parts(0, 4990))
 			.saturating_add(T::DbWeight::get().writes(1))
 	}
+
+	fn set_management_endpoint() -> Weight {
+		Weight::from_parts(56_280_000, 0)
+			.saturating_add(Weight::from_parts(0, 3655))
+			.saturating_add(T::DbWeight::get().reads(5))
+			.saturating_add(T::DbWeight::get().writes(6))
+	}
 }

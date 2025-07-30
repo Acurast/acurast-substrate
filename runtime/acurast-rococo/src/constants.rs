4d6baef60ddd34e8e5a6d909d025ec6d5cde3cfc
Mike Godenzi <m.godenzi@papers.ch>
2025-07-30 08:01:23 +0200

    fix: adjusted min/max candidates constants

--- runtime/acurast-rococo/src/constants.rs
+++ runtime/acurast-rococo/src/constants.rs
@@ -107,9 +107,9 @@ parameter_types! {
 	pub const Offset: u32 = 0;
 	pub const MaxAuthorities: u32 = 100_000;
 	pub const PotId: PalletId = PalletId(*b"PotStake");
-	pub const MaxCandidates: u32 = 1000;
-	pub const MinCandidates: u32 = 5;
-	pub const SessionLength: BlockNumber = 6 * HOURS;
+	pub const MaxCandidates: u32 = 20;
+	pub const MinCandidates: u32 = 0;
+	pub const SessionLength: BlockNumber = 6 * MINUTES;
 	pub const MaxInvulnerables: u32 = 100;
 	pub const ExecutiveBody: BodyId = BodyId::Executive;
 	pub Admins: Vec<AccountId> = vec![];

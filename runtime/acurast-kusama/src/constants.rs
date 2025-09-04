45fe37bf748564afec266ff481777d307162460a
Mike Godenzi <m.godenzi@papers.ch>
2025-09-04 09:54:01 +0200

    chore: updated versions

--- runtime/acurast-kusama/src/constants.rs
+++ runtime/acurast-kusama/src/constants.rs
@@ -21,7 +21,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_std::borrow::Cow::Borrowed("acurast-parachain"),
 	impl_name: sp_std::borrow::Cow::Borrowed("acurast-parachain"),
 	authoring_version: 1,
-	spec_version: 31,
+	spec_version: 32,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,

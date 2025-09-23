6dc48187ba1d27aa2cf61866a09e7ccc9b8b69ff
Mike Godenzi <m.godenzi@papers.ch>
2025-09-23 08:33:55 +0200

    chore: reverted epoch change

--- runtime/acurast-rococo/src/pallets/pallet_acurast_compute_config.rs
+++ runtime/acurast-rococo/src/pallets/pallet_acurast_compute_config.rs
@@ -24,7 +24,7 @@ use crate::{
 use pallet_acurast::ManagerProviderForEligibleProcessor;
 
 parameter_types! {
-	pub const Epoch: BlockNumber = 10; // 1.5 hours
+	pub const Epoch: BlockNumber = 900; // 1.5 hours
 	pub const Era: BlockNumber = 1; // 1.5 hours, only for testing, is normally 24 hours
 	pub const MetricEpochValidity: BlockNumber = 16 * 90; // 3 months, will be changed to 24 hours in the future
 	pub const WarmupPeriod: BlockNumber = 10; // 3 hours, only for testing, we should use something like 2 weeks = 219027

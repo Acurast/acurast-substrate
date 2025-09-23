2cf9a6b877147efdfbb320cb55d852b1945287ad
Mike Godenzi <m.godenzi@papers.ch>
2025-09-23 08:32:17 +0200

    fix: inflation is now specified as a fixed amount per epoch

--- runtime/acurast-rococo/src/pallets/pallet_acurast_compute_config.rs
+++ runtime/acurast-rococo/src/pallets/pallet_acurast_compute_config.rs
@@ -24,21 +24,21 @@ use crate::{
 use pallet_acurast::ManagerProviderForEligibleProcessor;
 
 parameter_types! {
-	pub const Epoch: BlockNumber = 900; // 1.5 hours
+	pub const Epoch: BlockNumber = 10; // 1.5 hours
 	pub const Era: BlockNumber = 1; // 1.5 hours, only for testing, is normally 24 hours
 	pub const MetricEpochValidity: BlockNumber = 16 * 90; // 3 months, will be changed to 24 hours in the future
 	pub const WarmupPeriod: BlockNumber = 10; // 3 hours, only for testing, we should use something like 2 weeks = 219027
 	pub const MaxMetricCommitmentRatio: Perquintill = Perquintill::from_percent(80);
 	pub const MinCooldownPeriod: BlockNumber = 10; // 10 blocks (for testing purposes)
 	pub const MaxCooldownPeriod: BlockNumber = 3600; // ~1 hour
-	pub const MinDelegation: Balance = 1 * UNIT;
+	pub const MinDelegation: Balance = UNIT;
 	pub const MaxDelegationRatio: Perquintill = Perquintill::from_percent(90);
 	pub const CooldownRewardRatio: Perquintill = Perquintill::from_percent(50);
-	pub const MinStake: Balance = 1 * UNIT;
+	pub const MinStake: Balance = UNIT;
 	pub const ComputeStakingLockId: LockIdentifier = *b"compstak";
 	pub const Decimals: Balance = UNIT;
 	pub const ComputePalletId: PalletId = PalletId(*b"cmptepid");
-	pub InflationPerDistribution: Perquintill = Perquintill::from_rational(835_451_506_486_784u128, 1_000_000_000_000_000_000u128);
+	pub const InflationPerEpoch: Balance = 8_561_643_835_616_438; // ~ 5% a year for a total supply of 1B
 	pub const InflationStakedBackedRation: Perquintill = Perquintill::from_percent(70);
 }
 
@@ -70,7 +70,7 @@ impl pallet_acurast_compute::Config for Runtime {
 		AcurastProcessorManager,
 		AcurastProcessorManager,
 	>;
-	type InflationPerDistribution = InflationPerDistribution;
+	type InflationPerEpoch = InflationPerEpoch;
 	type InflationStakedBackedRation = InflationStakedBackedRation;
 	type CreateModifyPoolOrigin = EnsureRoot<Self::AccountId>;
 	type OperatorOrigin = EnsureRoot<Self::AccountId>;

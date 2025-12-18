7007a0f093a0526e40b8bb7f85083e297c7a9aa6
Simon Wehrli <simon@smartnuance.com>
2025-12-18 17:03:31 +0100

    fix: allow manager to accept different committer
    if no commitment is active

--- runtime/acurast-kusama/src/constants.rs
+++ runtime/acurast-kusama/src/constants.rs
@@ -21,7 +21,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: sp_std::borrow::Cow::Borrowed("acurast-parachain"),
 	impl_name: sp_std::borrow::Cow::Borrowed("acurast-parachain"),
 	authoring_version: 1,
-	spec_version: 44,
+	spec_version: 45,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
